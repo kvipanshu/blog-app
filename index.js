98dfed2fd573ea57fbc254c87cab471b5fcbd9a2
revising <vipanshu.srivastava28@gmail.com>
2024-08-16 23:07:39 +0530

    edited

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 mongoose
-	.connect("mongodb://127.0.0.1:27017/blog-app")
+	.connect("mongodb://localhost:27017/blog-app")
 	.then((e) => console.log("MongoDB Connected"));
 
 app.set("view engine", "ejs");
